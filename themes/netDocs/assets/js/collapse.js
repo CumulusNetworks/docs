524ec1e475742784bc6697f4e80d0734303fcfd2
Lyndsay Crowther <lyndsay@Lyndsays-MBP.lan>
2019-07-10 15:26:47 +0200

    collapse/expand functionality

--- themes/netDocs/assets/js/collapse.js
+++ themes/netDocs/assets/js/collapse.js
@@ -1,43 +1,37 @@
 document.querySelectorAll('.cn-book-section-container').forEach(node => {
-  // if(node.querySelector('.active') != null){
-  //   node.setAttribute('class', '.cn-book-section-container expand')
-  // }else{
-  //   node.setAttribute('class', '.cn-book-section-container collapse')
-  // }
-
+ 
   if(node.querySelector('.active') == null){
     node.setAttribute('class', '.cn-book-section-container collapse')
   }
-
   if(node.parentNode.querySelector('.active') != null){
     node.setAttribute('class', '.cn-book-section-container expand')
   }
-
 
   eleToggler = document.createElement('input')
   eleToggler.setAttribute('type', 'image')
   eleToggler.setAttribute('class', 'menuControl')
   eleToggler.setAttribute('src', "/icons/baseline-expand_more.svg")
 
-  eleToggler.addEventListener('click', function(event){
-
-    if(node.getAttribute('class') == '.cn-book-section-container collapse'){
-      this.setAttribute('src', "/icons/baseline-expand_less.svg")
-      node.setAttribute('class', '.cn-book-section-container expand')
-    }else if(node.getAttribute('class') == '.cn-book-section-container expand'){
-      node.setAttribute('class', '.cn-book-section-container collapse')
-      this.setAttribute('src', "/icons/baseline-expand_more.svg")
-
-    }
-  })
-
-  if(node.querySelector('a') == null){
-    eleToggler.setAttribute('class', 'menuControl empty')
-  }
 
   let par = node.parentNode
   let lnk = par.querySelector('a')
   if(par.tagName == 'LI'){
     lnk.insertAdjacentElement('afterbegin',eleToggler)
+    lnk.addEventListener('click', function (e) {
+      if (lnk.classList.contains('active')) {
+        lnk.classList.remove('active')
+        lnk.nextElementSibling.classList.remove('expand')
+        lnk.nextElementSibling.classList.add('collapse')
+      }else {
+        lnk.classList.add('active')
+        lnk.nextElementSibling.classList.remove('collapse')
+        lnk.nextElementSibling.classList.add('expand')
+      }
+      e.preventDefault()
+    })
   }
-})
+});
+
+
+
+

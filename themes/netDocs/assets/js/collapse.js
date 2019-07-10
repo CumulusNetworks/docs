19fd1649777e3754d9dc3a831b77f4f6903144cc
Lyndsay Crowther <lyndsay@Lyndsays-MBP.lan>
2019-07-10 16:51:29 +0200

    reverting previous commit

--- themes/netDocs/assets/js/collapse.js
+++ themes/netDocs/assets/js/collapse.js
@@ -12,35 +12,12 @@ document.querySelectorAll('.cn-book-section-container').forEach(node => {
   eleToggler.setAttribute('class', 'menuControl')
   eleToggler.setAttribute('src', "/icons/baseline-expand_more.svg")
 
-  function load_url (url) {
-    fetch(url)
-    .then(function(response) {
-        // When the page is loaded convert it to text
-        return response.text()
-    })
-    .then(function(html) {
-        // Initialize the DOM parser
-        var parser = new DOMParser();
-
-        // Parse the text
-        var doc = parser.parseFromString(html, "text/html");
-        var content = doc.querySelector(".markdown").innerHTML;
-        document.querySelector(".markdown").innerHTML = content;
-    })
-    .catch(function(err) {  
-        console.log('Failed to fetch page: ', err);  
-    });
-} 
-
 
   let par = node.parentNode
   let lnk = par.querySelector('a')
-
   if(par.tagName == 'LI'){
     lnk.insertAdjacentElement('afterbegin',eleToggler)
     lnk.addEventListener('click', function (e) {
-      let url = lnk.getAttribute("href");
-
       if (lnk.classList.contains('active')) {
         lnk.classList.remove('active')
         lnk.nextElementSibling.classList.remove('expand')
@@ -50,7 +27,6 @@ document.querySelectorAll('.cn-book-section-container').forEach(node => {
         lnk.nextElementSibling.classList.remove('collapse')
         lnk.nextElementSibling.classList.add('expand')
       }
-      load_url(url)
       e.preventDefault()
     })
   }

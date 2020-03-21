398527a15dc08caef6c4e229272ac157c578e38f
Pete Lumbis <plumbis@cumulusnetworks.com>
2020-03-20 22:43:47 -0700

    set RNs to drafts

--- utils/build_rns.py
+++ utils/build_rns.py
@@ -135,6 +135,7 @@ def build_markdown_header(product, version):
     output.append("product: {}\n".format(product))
     output.append("version: \"{}\"\n".format(version))
     output.append("toc: 1\n")
+    output.append("draft: True\n")
     output.append("---\n")
     output.append("\n\n")
     

eab97d539d2cc18e3cd4560cf3046fb4e9a8c78c
Pete Lumbis <plumbis@cumulusnetworks.com>
2020-03-20 22:11:55 -0700

    check for git commit using diff-index

--- utils/travis-ci.sh
+++ utils/travis-ci.sh
@@ -1,30 +1,5 @@
 #!/bin/sh
-# Credit: https://gist.github.com/willprice/e07efd73fb7f13f917ea
-
-build_release_notes() {
-  git checkout plumbis-stage travis-docs
-  cd travis-docs
-  # Current month and year, e.g: Apr 2018
-  dateAndMonth=`date "+%b %Y"`
-  # run the python script to pull new RNs
-  python3 utils/build_rns.py
-  # Stage the modified files in dist/output
-  git add *
-  # Create a new commit with a custom build message
-  # with "[skip ci]" to avoid a build loop
-  # and Travis build number for reference
-  git commit -m "Auto commit of release note update on $dateAndMonth" -m "[skip ci]"
-}
-
-commit_release_notes() {
-  # Remove existing "origin"
-  git remote rm origin
-  # Add new "origin" with access token in the git URL for authentication
-  git remote add origin https://plumbis:${GH_TOKEN}@github.com/CumulusNetworks/docs.git > /dev/null 2>&1
-  git push origin plumbis-stage --quiet > /dev/null 2>&1
-}
-
-pwd 
+# Inspired by  https://gist.github.com/willprice/e07efd73fb7f13f917ea
 
 echo "making local_clone directory"
 mkdir local_clone
@@ -44,16 +19,14 @@ git checkout plumbis-stage
 echo "running rn script"
 python3 utils/build_rns.py 
 
-git add *
 
-# Attempt to commit to git only if "git commit" succeeded
-if [ $? -eq 0 ]; then
-  echo "Release note script detected release note updates. Commiting"
-  dateAndMonth=`date "+%b %Y"`
-  git commit -m "Auto commit of release note update on $dateAndMonth" -m "[skip ci]"
-  git push
-  exit $?
-else
+if git diff-index --quiet HEAD --; then
   echo "No release note updates."
   exit 0
+else
+  echo "Release note script detected release note updates. Commiting"
+  git add *
+  git commit -m "Auto commit of release note update" -m "[skip ci]"
+  git push
+  exit $?
 fi

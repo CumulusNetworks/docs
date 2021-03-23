58f39415d5bc4596bf836161b4854a7faa0f0ad2
Pete Lumbis <plumbis@nvidia.com>
2021-03-23 16:16:08 -0400

    additional xls output scrubbing

--- utils/build_rns.py
+++ utils/build_rns.py
@@ -230,10 +230,12 @@ def sanatize_rn_for_xls(string):
     output_string = output_string.replace('<div class=\"preformatted\" style=\"border-width: 1px;\"><div class=\"preformattedContent panelContent\">', "")
     output_string = output_string.replace("</div>", "")
 
-    # output_string = output_string.replace("&", "&amp;")
-    # output_string = output_string.replace("\"", "&quot;")
-    # output_string = output_string.replace("<", "&lt;")
-    # output_string = output_string.replace(">", "&gt;")
+    # NetQ-5774 Fix. The use of "<>" in a string inside a code (<pre>) block disappears
+    output_string = output_string.replace("<ipaddr>", "[ipaddr]")
+
+    output_string = output_string.replace("{noformat}", "")
+    output_string = output_string.replace("\\<", "&lt;")
+    output_string = output_string.replace("\\>", "&gt;")
 
     return output_string
 

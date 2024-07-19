057b1266269047e78ad5f54ea4ee0bbe0eb245b8
Stu Clark <sclark@nvidia.com>
2024-07-19 10:02:51 -0400

    add NetQ 4.11 FOSS update

--- utils/build_foss_licenses.py
+++ utils/build_foss_licenses.py
@@ -295,13 +295,13 @@ def get_products():
     '''
     # Some versions are included in the JSON file that don't have correct licenses
     # This is the list of versions to exclude from processing
-    cl_exclude_list = ["3.7.12", "4.1.0", "4.1.1", "4.2.0", "4.3.0", "4.3.1", "4.3.2", "4.4.0", "4.4.1", "4.4.1", "4.4.2", "4.4.3", "4.4.4", "4.4.5", "5.0.1", "5.1.0", "5.2.0", "5.3.0", "5.3.1", "5.4.0", "5.5.0", "5.5.1", "5.6.0", "5.7.0", "5.8.0", "5.9.0", "5.9.1"]
+    cl_exclude_list = ["3.7.12", "4.1.0", "4.1.1", "4.2.0", "4.3.0", "4.3.1", "4.3.2", "4.4.0", "4.4.1", "4.4.1", "4.4.2", "4.4.3", "4.4.4", "4.4.5", "5.0.1", "5.1.0", "5.2.0", "5.3.0", "5.3.1", "5.4.0", "5.5.0", "5.5.1", "5.6.0", "5.7.0", "5.8.0", "5.9.0", "5.9.1", "5.10.0"]
     netq_exclude_list = ["4.1.0", "4.2.0", "4.3.0", "4.4.0", "4.5.0", "4.6.0", "4.7.0", "4.8.0", "4.9.0", "4.10.0", "4.10.1"]
 
 
 
     session = requests.Session()
-    url = "https://d2whzysjlaya8k.cloudfront.net/release_notes_and_license_list.json"
+    url = "https://d2whzysjlaya8k.cloudfront.net/release_notes_and_license_list_stage.json"
     response = session.get(url)
     if response.status_code != 200:
         print("Unable to download JSON releases file to determine products and versions for FOSS licenses.")

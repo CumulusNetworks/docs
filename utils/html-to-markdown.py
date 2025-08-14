0806ee9571144a0491e651b56c30d7efdb844aa6
Stu Clark <sclark@nvidia.com>
2025-08-14 10:05:54 -0400

    add env variable to obfuscate amplify URL in repo, fix relurl path in replace

--- utils/html-to-markdown.py
+++ utils/html-to-markdown.py
@@ -4,6 +4,8 @@ from markdownify import markdownify
 
 html_files = glob.glob('public' + '/**/*.html', recursive=True)
 
+if("--url-replace" in sys.argv):
+    amp_url_replace = sys.argv[sys.argv.index("--url-replace") + 1]
 
 for file in html_files:
 
@@ -11,7 +13,7 @@ for file in html_files:
     file_contents = f.read()
     md_output = markdownify(file_contents)
 
-    edited_markdown=md_output.replace('](https://stu-stage.d30wkbbasyrvu1.amplifyapp.com', '](https://docs.nvidia.com').replace('](/networking-ethernet-software', '](https://docs.nvidia.com')
+    edited_markdown=md_output.replace(amp_url_replace, '](https://docs.nvidia.com').replace('](/networking-ethernet-software', '](https://docs.nvidia.com/networking-ethernet-software')
     
     with open(file + ".md", "a") as new_md_file:
         print(edited_markdown, file=new_md_file)

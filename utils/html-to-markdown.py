a505e37ff200aaf7a9ca2d11a62707fac6a7d0bb
Stu Clark <sclark@nvidia.com>
2025-08-14 09:37:09 -0400

    test amplify URL and relative link replacement in markdownify script

--- utils/html-to-markdown.py
+++ utils/html-to-markdown.py
@@ -10,6 +10,8 @@ for file in html_files:
     f = open(file, 'r')
     file_contents = f.read()
     md_output = markdownify(file_contents)
+
+    edited_markdown=md_output.replace('](https://stu-stage.d30wkbbasyrvu1.amplifyapp.com', '](https://docs.nvidia.com').replace('](/networking-ethernet-software', '](https://docs.nvidia.com')
     
     with open(file + ".md", "a") as new_md_file:
-        print(md_output, file=new_md_file)
+        print(edited_markdown, file=new_md_file)

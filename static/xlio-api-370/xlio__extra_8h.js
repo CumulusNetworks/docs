4e832dafa15eb5f80d360a94dc22a5862ebe3471
Dmytro Podgornyi <dmytrop@nvidia.com>
2026-05-04 16:30:05 +0300

    XLIO API 3.70
    Add XLIO Ultra API generated documentation for the 3.70.3 release.
    
    Signed-off-by: Dmytro Podgornyi <dmytrop@nvidia.com>

---
+++ static/xlio-api-370/xlio__extra_8h.js
@@ -0,0 +1,5 @@
+var xlio__extra_8h =
+[
+    [ "XLIO_MAGIC_NUMBER", "xlio__extra_8h.html#a525055e299f991d1b4ef4df1f1dea6c3", null ],
+    [ "__attribute__", "xlio__extra_8h.html#af169dac54f2eabc256ca2c7b1ab0344a", null ]
+];

c0e0c5a25b566d092b9b4343feabf45ef6d77b57
cumulusAnia <ania@nvidia.com>
2026-06-24 09:43:43 +0200

    update javascript for copy paste to prevent browser permission popup

--- themes/netDocs/assets/js/codecopy.js
+++ themes/netDocs/assets/js/codecopy.js
@@ -4,40 +4,43 @@ document.addEventListener('DOMContentLoaded', function () {
       var div = document.createElement('div');
       div.className = 'copy-code-img';
       div.type = 'button';
-      div.innerHTML = "<img src=\"https://icons.cumulusnetworks.com/01-Interface-Essential/29-Copy-Paste/copy-paste-1.svg\" width=\"20\" height=\"20\">";
+      div.innerHTML =
+        '<img src="https://icons.cumulusnetworks.com/01-Interface-Essential/29-Copy-Paste/copy-paste-1.svg" width="20" height="20">';
 
-      div.addEventListener('click', function (event) {
-        clipboard.writeText(codeBlock.innerText).then(function () {
+      div.addEventListener('click', function () {
+        // Remove shell prompts BEFORE copying
+        var cleanedText = codeBlock.innerText.replace(
+          /cumulus@.*?:~\$ /g,
+          ''
+        );
+
+        clipboard.writeText(cleanedText).then(
+          function () {
             div.blur();
-            $(div).attr('aria-label', 'Copied to clipboard!').addClass('tooltip');
+            $(div)
+              .attr('aria-label', 'Copied to clipboard!')
+              .addClass('tooltip');
+
             setTimeout(function () {
-                $(div).removeAttr('aria-label', 'Copied to clipboard!').removeClass('tooltip');
+              $(div)
+                .removeAttr('aria-label')
+                .removeClass('tooltip');
             }, 2000);
-        }, function (error) {
+          },
+          function (error) {
+            console.error('Copy failed:', error);
+
             div.blur();
             div.innerText = 'Error';
+
             setTimeout(function () {
               div.innerText = '';
-              div.innerHTML = "<img src=\"https://icons.cumulusnetworks.com/01-Interface-Essential/29-Copy-Paste/copy-paste-1.svg\" width=\"20\" height=\"20\">";
+              div.innerHTML =
+                '<img src="https://icons.cumulusnetworks.com/01-Interface-Essential/29-Copy-Paste/copy-paste-1.svg" width="20" height="20">';
             }, 2000);
-        });
-      async function cleanClipboard() {
-          try {
-              let text = await navigator.clipboard.readText();
-      
-              // Remove all matching shell prompts
-              text = text.replace(/cumulus@.*?:~\$ /g, "");
-      
-              await navigator.clipboard.writeText(text);
-              console.log("Clipboard updated:", text);
-          } catch (err) {
-              console.error("Failed to access clipboard:", err);
           }
-      }
-      
-      cleanClipboard();
-
-    });
+        );
+      });
 
       var pre = codeBlock.parentNode;
       pre.insertAdjacentElement('afterbegin', div);
@@ -48,14 +51,16 @@ document.addEventListener('DOMContentLoaded', function () {
     addCopyButtons(navigator.clipboard);
   } else {
     var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
-    script.integrity = 'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
+    script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/clipboard-polyfill/2.7.0/clipboard-polyfill.promise.js';
+    script.integrity =
+      'sha256-waClS2re9NUbXRsryKoof+F9qc1gjjIhc2eT7ZbIv94=';
     script.crossOrigin = 'anonymous';
-    script.onload = function() {
-        addCopyButtons(clipboard);
+
+    script.onload = function () {
+      addCopyButtons(clipboard);
     };
-  
+
     document.body.appendChild(script);
   }
 });
-
